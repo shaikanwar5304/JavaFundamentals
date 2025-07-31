3e8ace24808898d3d59e1ddc6cf6ad50e371219d
inwrsk <inwrsk@gmail.com>
2025-07-31 17:31:09 +0530

    java fundamentals revisit:

--- WiproPBL/A_JavaFundamentals/A_javaFundamentalsPracticeQ/LanguageBasics/StringConcatenation.java
+++ WiproPBL/A_JavaFundamentals/A_javaFundamentalsPracticeQ/LanguageBasics/StringConcatenation.java
@@ -1,5 +1,8 @@
+// simple concatenation
 public class StringConcatenation {
     public static void main(String[] args) {
         System.out.println(args[0]+" technologies "+args[1]);
     }
 }
+// java Stringconcatenation.java hello welcome
+// output: hello technologies welcome
